1b45527681f2424d07c2db97c91900ca5bf45013
ibad siddiqui <ibad siddiqui>
2016-10-07 21:17:14 +0500

    completed

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@
     var css1 = prompt('Please enter your CSS3 Test1 marks');
     var css2 = prompt('Please enter your CSS3 Test2 marks');
 
-    var result = (parseInt(html)+ parseInt(css1)+ parseInt(css2)+ parseInt(git))/4 * 100;
+    var result = (parseInt(html)+ parseInt(css1)+ parseInt(css2)+ parseInt(git))/400 * 100;
     document.getElementById('fname').innerHTML = fatherName;
     document.getElementById('name').innerHTML = userName;
     document.getElementById('roll').innerHTML = rollNo;
@@ -23,12 +23,12 @@
     document.getElementById('rslt').innerHTML =  result;
 
     if(result>80 && result < 100)
-        document.getElementById().innerHTML = 'Congrats, you got A+';
+        document.getElementById('remarks').innerHTML = 'Congrats, you got A+';
     else if(result>70 && result < 80)
-        document.getElementById().innerHTML = 'Nicely done, you got A';
+        document.getElementById('remarks').innerHTML = 'Nicely done, you got A';
     else if(result>60 && result < 70)
-        document.getElementById().innerHTML = 'You can do better, you got B';
-    else 
-        document.getElementById().innerHTML = 'Sorry, You are failed';
+        document.getElementById('remarks').innerHTML = 'You can do better, you got B';
+    else if(result<60)
+        document.getElementById('remarks').innerHTML = 'Sorry, You are failed';
         
 })();
