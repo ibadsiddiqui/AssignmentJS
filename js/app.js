c768bd179b0b5d1d50722465ed5eb8415abaf343
ibad siddiqui <ibad siddiqui>
2016-10-07 20:44:43 +0500

    logic for the web was created using javascript

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var userName = prompt('Please enter Your Name:','Your Name');
+var fatherName = prompt('Please enter Your Father Name:','Father Name');
+var rollNo = prompt('Please enter your roll number: ', 'Roll Number');
+var git = prompt('Please enter your Git marks');
+var html = prompt('Please enter your HTML5 marks');
+var css1 = prompt('Please enter your CSS3 Test1 marks');
+var css2 = prompt('Please enter your CSS3 Test2 marks');
+var result = (html+ css1+ css2+ git)/4 * 100;
+document.getElementById  
