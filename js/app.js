3d91a873a4c3034d31c36bfee9dbd31eaae55353
ibad siddiqui <ibad siddiqui>
2016-10-07 21:07:31 +0500

    Grading was added to the page

--- js/app.js
+++ js/app.js
@@ -1,9 +1,34 @@
-var userName = prompt('Please enter Your Name:','Your Name');
-var fatherName = prompt('Please enter Your Father Name:','Father Name');
-var rollNo = prompt('Please enter your roll number: ', 'Roll Number');
-var git = prompt('Please enter your Git marks');
-var html = prompt('Please enter your HTML5 marks');
-var css1 = prompt('Please enter your CSS3 Test1 marks');
-var css2 = prompt('Please enter your CSS3 Test2 marks');
-var result = (html+ css1+ css2+ git)/4 * 100;
-document.getElementById  
+(function(){
+    var userName = prompt('Please enter Your Name:','Your Name');
+
+    var fatherName = prompt('Please enter Your Father Name:','Father Name');
+
+    var rollNo = prompt('Please enter your roll number: ', 'Roll Number');
+
+    var git = prompt('Please enter your Git marks');
+
+    var html = prompt('Please enter your HTML5 marks');
+
+    var css1 = prompt('Please enter your CSS3 Test1 marks');
+    var css2 = prompt('Please enter your CSS3 Test2 marks');
+
+    var result = (parseInt(html)+ parseInt(css1)+ parseInt(css2)+ parseInt(git))/4 * 100;
+    document.getElementById('fname').innerHTML = fatherName;
+    document.getElementById('name').innerHTML = userName;
+    document.getElementById('roll').innerHTML = rollNo;
+    document.getElementById('git').innerHTML = git;
+    document.getElementById('html5').innerHTML = html;
+    document.getElementById('css31').innerHTML = css1;
+    document.getElementById('css32').innerHTML = css2;
+    document.getElementById('rslt').innerHTML =  result;
+
+    if(result>80 && result < 100)
+        document.getElementById().innerHTML = 'Congrats, you got A+';
+    else if(result>70 && result < 80)
+        document.getElementById().innerHTML = 'Nicely done, you got A';
+    else if(result>60 && result < 70)
+        document.getElementById().innerHTML = 'You can do better, you got B';
+    else 
+        document.getElementById().innerHTML = 'Sorry, You are failed';
+        
+})();
