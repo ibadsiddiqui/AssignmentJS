2e128bef2b16bbc905ece029fd0ea771f4a34bef
ibad siddiqui <ibad siddiqui>
2016-10-08 09:38:22 +0500

    grading was improved

--- js/app.js
+++ js/app.js
@@ -1,34 +1,34 @@
-// (function(){
-//     var userName = prompt('Please enter Your Name:','Your Name');
+(function(){
+    var userName = prompt('Please enter Your Name:','Your Name');
 
-//     var fatherName = prompt('Please enter Your Father Name:','Father Name');
+    var fatherName = prompt('Please enter Your Father Name:','Father Name');
 
-//     var rollNo = prompt('Please enter your roll number: ', 'Roll Number');
+    var rollNo = prompt('Please enter your roll number: ', 'Roll Number');
 
-//     var git = prompt('Please enter your Git marks');
+    var git = prompt('Please enter your Git marks');
 
-//     var html = prompt('Please enter your HTML5 marks');
+    var html = prompt('Please enter your HTML5 marks');
 
-//     var css1 = prompt('Please enter your CSS3 Test1 marks');
-//     var css2 = prompt('Please enter your CSS3 Test2 marks');
+    var css1 = prompt('Please enter your CSS3 Test1 marks');
+    var css2 = prompt('Please enter your CSS3 Test2 marks');
 
-//     var result = (parseInt(html)+ parseInt(css1)+ parseInt(css2)+ parseInt(git))/400 * 100;
-//     document.getElementById('fname').innerHTML = fatherName;
-//     document.getElementById('name').innerHTML = userName;
-//     document.getElementById('roll').innerHTML = rollNo;
-//     document.getElementById('git').innerHTML = git;
-//     document.getElementById('html5').innerHTML = html;
-//     document.getElementById('css31').innerHTML = css1;
-//     document.getElementById('css32').innerHTML = css2;
-//     document.getElementById('rslt').innerHTML =  result;
+    var result = (parseInt(html)+ parseInt(css1)+ parseInt(css2)+ parseInt(git))/400 * 100;
+    document.getElementById('fname').innerHTML = fatherName;
+    document.getElementById('name').innerHTML = userName;
+    document.getElementById('roll').innerHTML = rollNo;
+    document.getElementById('git').innerHTML = git;
+    document.getElementById('html5').innerHTML = html;
+    document.getElementById('css31').innerHTML = css1;
+    document.getElementById('css32').innerHTML = css2;
+    document.getElementById('rslt').innerHTML =  result;
 
-//     if(result>80 && result < 100)
-//         document.getElementById('remarks').innerHTML = 'Congrats, you got A+';
-//     else if(result>70 && result < 80)
-//         document.getElementById('remarks').innerHTML = 'Nicely done, you got A';
-//     else if(result>60 && result < 70)
-//         document.getElementById('remarks').innerHTML = 'You can do better, you got B';
-//     else if(result<60)
-//         document.getElementById('remarks').innerHTML = 'Sorry, You are failed';
+    if(result>80 && result < 100)
+        document.getElementById('remarks').innerHTML = 'Congrats, you got A+';
+    else if(result>70 && result < 80)
+        document.getElementById('remarks').innerHTML = 'Nicely done, you got A';
+    else if(result>60 && result < 70)
+        document.getElementById('remarks').innerHTML = 'You can do better, you got B';
+    else if(result<60)
+        document.getElementById('remarks').innerHTML = 'Sorry, You are failed';
         
-// })();
+})();
